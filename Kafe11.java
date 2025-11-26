f860941e5c238cff17a69a0885b9a41731243342
nauravlrie <nauravlrie@gmail.com>
2025-11-26 08:37:55 +0700

    first commit

---
+++ Kafe11.java
@@ -0,0 +1,21 @@
+public class Kafe11 {
+    public static void Menu () {
+        System.out.println("===== MENU RESTO KAFE =====");
+        System.out.println("1. Kopi Hitam - Rp 15.000");
+        System.out.println("2. Cappuccino - Rp 20.000");
+        System.out.println("3. Latte - Rp 22.000");
+        System.out.println("4. Teh Tarik - Rp 12.000");
+        System.out.println("5. Roti Bakar - Rp 10.000");
+        System.out.println("6. Mie Goreng - Rp 18.000");
+        System.out.println("==========================");
+        System.out.println("Silakan pilih menu yang Anda inginkan.");
+        System.out.println(); //baris kosong agar  rapi
+    }
+    //fungsi main program akan dimulai dari sini
+    public static void main(String[] args) {
+        //untuk memanggil fungsi menu yang tadi dibuat
+        Menu();
+    }
+}
+        
+   
